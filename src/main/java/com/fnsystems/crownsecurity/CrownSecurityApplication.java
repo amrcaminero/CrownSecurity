96bf523a3e8fe7fe34ca5f3e1f715eb3a258c1dd
Alexa <amrcaminero@gmail.com>
2018-03-27 14:26:58 +0900

    first commit

---
+++ src/main/java/com/fnsystems/crownsecurity/CrownSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.fnsystems.crownsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrownSecurityApplication.class, args);
+	}
+}
